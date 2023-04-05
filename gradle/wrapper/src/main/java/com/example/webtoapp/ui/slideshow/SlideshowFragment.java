753ecbe34e29db58adce0e131b4aa54506bee40f
Ranjanaakash <126784585+Ranjanaakash@users.noreply.github.com>
2023-04-05 10:53:25 +0530

    Add files via upload

---
+++ gradle/wrapper/src/main/java/com/example/webtoapp/ui/slideshow/SlideshowFragment.java
@@ -0,0 +1,69 @@
+package com.example.webtoapp.ui.slideshow;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.webtoapp.databinding.FragmentSlideshowBinding;
+
+public class SlideshowFragment extends Fragment {
+
+    private FragmentSlideshowBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        SlideshowViewModel slideshowViewModel =
+                new ViewModelProvider(this).get(SlideshowViewModel.class);
+
+        binding = FragmentSlideshowBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final WebView PracticeWebView = binding.idWebViewPractice;
+        final ProgressBar loadingPB = binding.idPBLoading;
+        PracticeWebView.loadUrl("https://converterio.pythonanywhere.com/englishus");
+        PracticeWebView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                loadingPB.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                loadingPB.setVisibility(View.GONE);
+            }
+        });
+        PracticeWebView.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent event) {
+                if (event.getAction() == KeyEvent.ACTION_DOWN)
+                    switch (i) {
+                        case KeyEvent.KEYCODE_BACK:
+                            if (PracticeWebView.canGoBack()) {
+                                PracticeWebView.goBack();
+                            }
+                    }
+                return false;
+            }
+        });
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
